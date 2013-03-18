d5b09542dc86e5936d0b3fe17e37db690a69406e
Nicolas Dandrimont <nicolas.dandrimont@crans.org>
2013-03-18 22:44:00 +0100

    Test the GPGKey model

--- mentors/profiles/tests/test_models.py
+++ mentors/profiles/tests/test_models.py
@@ -30,9 +30,13 @@
 
 from __future__ import unicode_literals
 
-from django.test import TestCase
+import os
 
-from profiles.models import MentorsUser
+from django.conf import settings
+
+from lib.test import TestCase
+from lib.gnupg import GnuPG
+from profiles.models import GPGKey, MentorsUser
 
 
 class MentorsUserTests(TestCase):
@@ -63,3 +67,85 @@ class MentorsUserTests(TestCase):
     def test_user_create_empty_email(self):
         with self.assertRaises(ValueError):
             MentorsUser.objects.create_user('')
+
+
+class GPGKeyTests(TestCase):
+    #flake8: noqa
+    def setUp(self):
+        self.nicolas = MentorsUser.objects.create_user('nicolas@dandrimont.eu')
+        self.nicolas.save()
+        self.clement = MentorsUser.objects.create_user('clement@mux.me')
+        self.clement.save()
+        self.random = MentorsUser.objects.create_user('whomever@example.com')
+        self.random.save()
+
+        nicolas_key_path = os.path.join(os.path.dirname(__file__), 'data', 'dandrimont.asc')
+        self.nicolas_key = file(nicolas_key_path).read()
+        self.nicolas_key_fingerprint = '791F12396630DD71FD364375B8E5087766475AAF'
+        self.nicolas_key_algorithm = '4096R'
+
+        clement_key_path = os.path.join(os.path.dirname(__file__), 'data', 'schreiner.asc')
+        self.clement_key = file(clement_key_path).read()
+        self.clement_key_fingerprint = 'E4BA6F4097B08D5AE8DC68C95E39E0E38123F27C'
+        self.clement_key_algorithm = '4096R'
+
+    def test_gpg_right_user(self):
+        new_key = GPGKey(owner=self.nicolas)
+        new_key.key = self.nicolas_key
+        new_key.save()
+
+        self.assertEquals(new_key.fingerprint, self.nicolas_key_fingerprint)
+        self.assertEquals(new_key.algorithm, self.nicolas_key_algorithm)
+
+    def test_gpg_wrong_user(self):
+        from django.core.exceptions import ValidationError
+
+        new_key = GPGKey(owner=self.random)
+        new_key.key = self.nicolas_key
+        with self.assertRaises(ValidationError):
+            new_key.save()
+
+    def test_gpg_wrong_data(self):
+        from django.core.exceptions import ValidationError
+
+        new_key = GPGKey(owner=self.random)
+        new_key.key = "blablablablabla"
+        with self.assertRaises(ValidationError):
+            new_key.save()
+
+    def test_gpg_display(self):
+        new_key = GPGKey(owner=self.nicolas)
+        new_key.key = self.nicolas_key
+        new_key.save()
+
+        self.assertIn(new_key.algorithm, unicode(new_key))
+        self.assertIn(new_key.fingerprint[-16:], unicode(new_key))
+
+    def test_gpg_keyring(self):
+        nicolas_key = GPGKey(owner=self.nicolas)
+        nicolas_key.key = self.nicolas_key
+        nicolas_key.save()
+
+        clement_key = GPGKey(owner=self.clement)
+        clement_key.key = self.clement_key
+        clement_key.save()
+
+        keyring_path = os.path.join(settings.MENTORS_ROOT, 'gpg/pubring.gpg')
+        self.assertTrue(os.path.exists(keyring_path))
+
+        gpg = GnuPG(default_keyring=keyring_path)
+
+        keys = list(gpg.list_keys())
+        self.assertEqual(len(keys), 2)
+
+        fprs = [key.key.fingerprint for key in keys]
+        self.assertIn(self.nicolas_key_fingerprint, fprs)
+        self.assertIn(self.clement_key_fingerprint, fprs)
+
+        clement_key.delete()
+
+        keys = list(gpg.list_keys())
+        self.assertEqual(len(keys), 1)
+
+        fprs = [key.key.fingerprint for key in keys]
+        self.assertIn(self.nicolas_key_fingerprint, fprs)
