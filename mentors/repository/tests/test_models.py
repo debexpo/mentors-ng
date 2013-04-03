f136217d5b87d9dde5ea1a25004db8d457e64545
Clément Schreiner <clement@mux.me>
2013-04-04 00:37:06 +0200

    Add tests/ directory.

---
+++ mentors/repository/tests/test_models.py
@@ -0,0 +1,34 @@
+from __future__ import unicode_literals
+
+import os
+
+from django.conf import settings
+
+from lib.test import TestCase
+
+from repository.models import Package, PackageUpload, BinaryPackage, SourcePackage, PackageFile
+
+class PackageTests(TestCase):
+    #flake8: noqa
+    pass
+
+
+class PackageUploadTests(TestCase):
+    #flake8: noqa
+    pass
+
+
+class BinaryPackageTests(TestCase):
+    #flake8: noqa
+    pass
+
+
+class SourcePackageTests(TestCase):
+    #flake8: noqa
+    pass
+
+
+class PackageFileTests(TestCase):
+    #flake8: noqa
+    pass
+
