dd89e4211d397437145974dc8632efbcdf30eac0
Clément Schreiner <clement@mux.me>
2013-04-04 20:12:22 +0200

    Add __unicode__ methods.

--- mentors/repository/models.py
+++ mentors/repository/models.py
@@ -34,7 +34,6 @@ from django.conf import settings
 class Package(models.Model):
     name = models.TextField()
     uploader = models.ForeignKey(settings.AUTH_USER_MODEL)
-
     version = models.TextField()
     maintainer = models.TextField()
     section = models.TextField()
@@ -46,14 +45,31 @@ class Package(models.Model):
     closes = models.TextField()
     upload_time = models.DateTimeField()
 
+    def __unicode__(self):
+        return '{0}-{1} in {2} (by {3})'.format(self.name,
+                                                self.version,
+                                                self.suite,
+                                                self.uploader)
+
 
 class BinaryPackage(models.Model):
+    name = models.TextField()
     package = models.ForeignKey(Package)
     arch = models.TextField()
     description = models.TextField()
 
+    def __unicode__(self):
+        return '{0} ({1})'.format(self.name,
+                                  self.arch)
+
+
 
 class SourcePackage(models.Model):
+    name = models.TextField()
+    package = models.ForeignKey(PackageUpload)
+
+    def __unicode__(self):
+        return self.name
 
 
 class PackageFile(models.Model):
@@ -63,6 +79,13 @@ class PackageFile(models.Model):
     size = models.IntegerField()
     checksum = models.CharField(max_length=200)
 
+    def __unicode__(self):
+        return '{0} ({1})'.format(self.filename,
+                                  self.binary if self.binary else self.source)
+
 
 class Suite(models.Model):
     name = models.TextField()
+
+    def __unicode__(self):
+        return self.name
